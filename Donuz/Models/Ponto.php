c9c985858845b8e9fbd88229ede5d47df4a37348
Robson Alves <robsonalves.dev@gmail.com>
2017-07-24 10:39:43 -0300

    adicionado requisição inserir pontos

--- Donuz/Models/Ponto.php
+++ Donuz/Models/Ponto.php
@@ -14,4 +14,16 @@ class Ponto
     {
         return Curl::get('ponto/' . Curl::$estabelecimento_id, ['Token: ' . Curl::$token, 'Token-Cliente: ' . $tokenCliente]);
     }
+
+    /**
+     * @param $data
+     *
+     * @return mixed
+     */
+    public function insertPoints($data)
+    {
+        $data = array_merge(['acao' => 'inserir', 'estabelecimento' => Curl::$estabelecimento_id], $data);
+
+        return Curl::post('ponto', ['Token: ' . Curl::$token], $data);
+    }
 }
